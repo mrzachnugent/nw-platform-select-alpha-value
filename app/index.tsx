ffbf0a88c12c6f7db038446b2f522405fc673969
Zach Nugent <mrzachnugent@gmail.com>
2024-03-27 12:56:02 -0400

    minimal repoduction bug: platform select color for second platform with alpha value

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 
 export default function Screen() {
   return (
-    <View className='flex-1 justify-center items-center bg-secondary/30'>
+    <View className='flex-1 justify-center items-center bg-background/30'>
       <Text>Screen</Text>
     </View>
   );
