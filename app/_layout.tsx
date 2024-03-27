71fb8105c6983bc1c45b5fd36576ed37b80a29c0
Zach Nugent <mrzachnugent@gmail.com>
2024-03-27 12:45:27 -0400

    init

---
+++ app/_layout.tsx
@@ -0,0 +1,8 @@
+import '~/global.css';
+
+import { Stack } from 'expo-router';
+import * as React from 'react';
+
+export default function RootLayout() {
+  return <Stack />;
+}
---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Screen() {
+  return (
+    <View className='flex-1 justify-center items-center bg-secondary/30'>
+      <Text>Screen</Text>
+    </View>
+  );
+}
